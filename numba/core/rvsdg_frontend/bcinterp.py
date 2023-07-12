2311fe830d62cd7475614f44cb7c3d3210274ee1
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2023-07-12 16:48:04 -0500

    Reduce number of variables needed.

--- numba/core/rvsdg_frontend/bcinterp.py
+++ numba/core/rvsdg_frontend/bcinterp.py
@@ -469,6 +469,10 @@ class RVSDG2IR(RegionVisitor[_Data]):
 
     def store(self, value, name, *, redefine=True, block=None) -> ir.Var:
         target: ir.Var
+        # The following `if` is to reduce the number of assignments
+        if isinstance(value, ir.Var) and redefine and name.startswith("$"):
+            return value
+
         if redefine:
             target = self.scope.redefine(name, loc=self.loc)
         else:
